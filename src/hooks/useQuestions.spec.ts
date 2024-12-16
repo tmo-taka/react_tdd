10dba05d8e7706d5784da6d4184a15d1d7c9c060
tmo-taka <45462276+tmo-taka@users.noreply.github.com>
2024-12-16 23:38:18 +0900

    fix(hooks): add missing dependency in useCallback hook

--- src/hooks/useQuestions.spec.ts
+++ src/hooks/useQuestions.spec.ts
@@ -1,9 +1,33 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useQuestions } from './useQuestions';
+import { formattedEnglishArr } from '../__test__/formattedEnglishArr';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('test useEnglishArr', () => {
-  it('should return an array of English words', () => {
+  it('should return state of EnglishArr when set EnglishArr', () => {
     const { result } = renderHook(() => useQuestions());
+    act(() => {
+      result.current.setEnglishArr(formattedEnglishArr);
+    });
     expect(result.current.englishArr).toBeDefined();
+    expect(result.current.englishArr).toHaveLength(10);
+    expect(Object.keys(result.current.englishArr[1])).toEqual(
+      expect.arrayContaining(['word', 'japanese']),
+    );
+  });
+
+  it('should set correctFlag is true if given inputted text is current', () => {
+    const { result } = renderHook(() => useQuestions());
+    act(() => {
+      result.current.setEnglishArr(formattedEnglishArr);
+    });
+
+    act(() => {
+      result.current.checkedAnswer('get', '得る(受動態にできない)');
+    });
+    expect(result.current.correctFlag).toBe(true);
   });
 });
--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -1,7 +1,22 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import type { EnglishArr } from '../domain/englishArr';
 
 export const useQuestions = () => {
   const [englishArr, setEnglishArr] = useState<EnglishArr>([]);
-  return { englishArr, setEnglishArr };
+  const [correctFlag, setCorrectFlag] = useState(false);
+
+  const checkedAnswer = useCallback(
+    (word: string, inputtedText: string) => {
+      const targetIndex = englishArr.findIndex(
+        (english) => english.word === word,
+      );
+      for (const correctText of englishArr[targetIndex].japanese) {
+        if (correctText === inputtedText) {
+          setCorrectFlag(true);
+        }
+      }
+    },
+    [englishArr],
+  );
+  return { englishArr, correctFlag, setEnglishArr, checkedAnswer };
 };
