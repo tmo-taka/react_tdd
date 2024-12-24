5f4a6e5872f3712c16ef9c377893b23eb6f5e7bb
tmo-taka <45462276+tmo-taka@users.noreply.github.com>
2024-12-24 09:54:13 +0900

    feat: add jotai-devtools setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'jotai';
+import { DevTools } from 'jotai-devtools';
 import './index.css';
 import App from './App.tsx';
 
@@ -8,8 +10,11 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider>
+      <DevTools />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
